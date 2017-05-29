ca02f49042fa415a18f9712bf2c103c3e3552c4c
Arindam Biswas <aribis369@gmail.com>
2017-05-29 17:56:18 +0800

    Done required changes.
    Added webhook bot, conformation mail & used environment variables.

--- app.py
+++ app.py
@@ -4,51 +4,67 @@ import os
 import json
 import requests
 from multiprocessing import Pool
-from slackclient import SlackClient
+
 
 app = Flask(__name__)
 
 
 def send_mail(form_msg, form_name, form_email):
     print("inside async")
-    app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-    app.config['MAIL_PORT'] = 587  # 465 for SSL
-    app.config['MAIL_USE_TLS'] = True
-    app.config['MAIL_USE_SSL'] = False  # True
-    app.config['MAIL_USERNAME'] = 'dibyadasiscool@gmail.com'
-    app.config['MAIL_PASSWORD'] = 'samsunghp'
+    app.config['MAIL_SERVER'] = os.environ['MAIL_SERVER']
+    app.config['MAIL_PORT'] = os.environ['MAIL_PORT']  # 465 for SSL
+    app.config['MAIL_USE_TLS'] = os.environ['MAIL_USE_TLS']
+    app.config['MAIL_USE_SSL'] = os.environ['MAIL_USE_SSL']  # True
+    app.config['MAIL_USERNAME'] = os.environ['MAIL_USERNAME']
+    app.config['MAIL_PASSWORD'] = os.environ['MAIL_PASSWORD']
     print("all config done")
     mail = Mail(app)
-    print("init mail app")
+    print("init KOSS mail app")
+
+    # query mail to KOSS
     form_data = form_msg  # request.form['msg']
     print("message taken")
     msg = Message('Query', sender='dibyadasiscool@gmail.com',
-                  recipients=['dibyadas998@gmail.com'])
+                  recipients=[os.environ['KOSS_EMAIL_ID']])
     print("message init")
-    msg.body = "Query sent by:- " + form_name + "\n" + \
-        form_data + "\n" + "Email-ID is:- " + form_email
+    msg.body = "Query sent by:- " + form_name + "\n\n" +\
+        form_data + "\n\n" + "Email-ID is:- " + form_email
     print("body made")
     print("going to send the msg")
     mail.send(msg)
     print("sent")
+
+    # conformation mail to user
+    print("message taken")
+    msg = Message('Query Recieved', sender='dibyadasiscool@gmail.com',
+                  recipients=[form_email])
+    print("message init")
+    msg.body = "Hi "+sender_name+",\n\n"+"Your message has been recieved by us and we will responded to it soon."+"\nThank-you for communicating with us, hope your query will cleared by our team."+"\n\n\nKOSS IIT Kharagpur"
+    print("body made")
+    print("going to send the msg")
+    mail.send(msg)
+    print("sent")
+
     slack_notifier(form_msg, form_name, form_email)
     return None
 
 
 def slack_notifier(form_msg, form_name, form_email,count=0):
-    # pl. enter a valid api key for KOSS slack bot
-    api_key="xoxb-18hdfbvhhjd-dODofrfhjbfdjhbfdjhbdjh"
-    sc=SlackClient(api_key)
+    headers = {"Content-Type": "application/json"}
 
-    try:
-        sc.api_call("chat.postMessage",channel="#queries",text="Query from "+form_name+"<"+form_email+">\n"+"QUERY : "+form_msg+"\nPlease respond soon.")
-    except:
-        if count<=2: # trys 3 times before termination
-            print("error!!! trying again")
+    data = json.dumps({"text": "Query from "+form_name+"<"+form_email+">\n\n"+"QUERY : "+form_msg+"\n\nPlease respond soon."})
+       
+    r = requests.post(os.environ["SLACK_WEBHOOK_URL"], headers=headers, data=data)
+
+    if r.status_code!=200:
+        if count<=2:
+            print("error "+r.status_code+" !!! trying again")
             count+=1
             slack_notifier(form_msg, form_name, form_email,count)
         else:
             print("terminated!!!")
+    else:
+        print("notification sent!!!")
 
         
         
